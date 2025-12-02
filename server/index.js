d3cae46f6b84bf80942ccc8189bf766a79f7e638
Atulya <snehitms7@gmail.com>
2025-12-02 19:23:32 +0530

    index.js comment removed

--- server/index.js
+++ server/index.js
@@ -21,7 +21,6 @@ app.use(cors());
 app.use(express.json());
 
 // Rate Limiting
-// Rate Limiting
 // const rateLimit = require('express-rate-limit');
 // const limiter = rateLimit({
 //     windowMs: 15 * 60 * 1000, // 15 minutes
