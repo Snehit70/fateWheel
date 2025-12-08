aded96856fb25c878da386e7afbdfb9b670daa25
Atulya <snehitms7@gmail.com>
2025-12-08 21:56:16 +0530

    default to 11

--- client/src/components/BettingControls.vue
+++ client/src/components/BettingControls.vue
@@ -29,14 +29,27 @@
             <div class="relative flex-1">
                 <span class="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 font-bold text-xl z-10"></span>
                 <Input 
-                    type="number" 
+                    type="text" 
+                    inputmode="numeric"
                     v-model.number="betAmount"
-                    class="pl-10 pr-4 font-mono text-xl sm:text-2xl font-bold h-10 sm:h-14"
+                    @keypress="onlyAllowNumbers"
+                    class="pl-10 pr-10 font-mono text-xl sm:text-2xl font-bold h-10 sm:h-14 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                     placeholder="0"
                     :min="10"
                     :max="1000"
                     :disabled="!isLoggedIn || isSpinning"
                 />
+                <button 
+                    v-if="betAmount > 0"
+                    @click="betAmount = 0"
+                    class="absolute right-3 top-1/2 -translate-y-1/2 text-text-muted hover:text-white transition-colors z-10"
+                    :disabled="!isLoggedIn || isSpinning"
+                    type="button"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                    </svg>
+                </button>
             </div>
             <div class="flex gap-1">
                  <Button variant="secondary" size="sm" @click="setAmount('10')" class="h-10 sm:h-14 px-2 sm:px-4 text-xs sm:text-sm font-bold" :disabled="!isLoggedIn || isSpinning">10</Button>
@@ -117,7 +130,7 @@ const props = defineProps({
 
 const emit = defineEmits(['update:amount', 'clear-input', 'clear-bets', 'spin']);
 
-const betAmount = ref(0);
+const betAmount = ref(11);
 
 const chips = [
     { label: '11', value: 11 },
@@ -152,6 +165,13 @@ const addAmount = (amount) => {
         betAmount.value = newAmount;
     } else {
         betAmount.value = 1000;
+    }
+};
+
+const onlyAllowNumbers = (event) => {
+    const char = String.fromCharCode(event.which || event.keyCode);
+    if (!/[0-9]/.test(char)) {
+        event.preventDefault();
     }
 };
 </script>
